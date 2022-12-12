52f1a8d14b507270481da7bd6a7d777f83d92987
YogitaDevi <yogita.gry@gmail.com>
2022-12-12 22:22:22 +0530

    Management_Program

---
+++ JSONInventory_UC2/JSONMain.cs
@@ -0,0 +1,30 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JSONInventory_UC2
+{
+    internal class JSONMain
+    {
+        model Inventory = new model();
+        List<model> InventoryList = new List<model>();
+        public void convert(string jFilePath)
+        {
+            using (StreamReader reader = new StreamReader(jFilePath))
+            {
+                var json = reader.ReadToEnd();
+                var items = JsonConvert.DeserializeObject<List<model>>(json);
+                Console.WriteLine("name weight price total value");
+                foreach (var item in items)
+                {
+                    Double value = item.price * item.weight;
+                    Console.WriteLine(item.name + " " + item.weight + " " + item.price + " " + value);
+
+                }
+            }
+        }
+    }
+}
---
+++ JSONInventory_UC2/Program.cs
@@ -0,0 +1,4 @@
+﻿
+JSONInventory_UC2.JSONMain jSON = new JSONInventory_UC2.JSONMain();
+jSON.convert("E:\\RFP231\\JSON_Programs\\JSONInventory_UC2\\Inventory.json");
+
